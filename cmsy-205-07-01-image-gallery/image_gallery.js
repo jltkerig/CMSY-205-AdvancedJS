cf9b29140d873f71cbd3b11c9838d0f97ebd79fe
Jamie <jltsantara@gmail.com>
2019-10-10 17:03:22 -0400

    instructions added

---
+++ cmsy-205-07-01-image-gallery/image_gallery.js
@@ -0,0 +1,11 @@
+/* 
+All necessary HTML, CSS, and image files for this application are provided in a zip format. You’ll also find an empty JavaScript file named image_gallery.js.  You can add your code to this file. 
+
+In the JavaScript file, add an event handler for the ready event method.
+
+Use the each method to run a function for each <a> element in the unordered list of items. Then, add jQuery code that gets the URL and caption for each image and preloads the image. You can get the URL from the href attribute of the <a> element, and you can get the caption from the title attribute.
+
+Add an event handler for the click event of each link. The function for this event handler should accept a parameter named evt. The jQuery code for this event handler should display the image and caption for the link that was clicked. In addition, it should use the evt parameter to cancel the default action of the link.
+
+Add a jQuery statement that moves the focus to the first link on the page when the page is loaded.
+*/
---
+++ cmsy-205-07-02-expand-collaspe/subset_expansion.js
@@ -0,0 +1,10 @@
+/*
+In this exercise, you’ll develop an application that displays the first paragraph of text for three topics and then lets the user click a link to expand or collapse the text for each topic.
+
+All necessary HTML, CSS, and image files for this application are provided in a zip format.  Then, run the application to see that the first paragraph of text is displayed for each topic, along with a link that lets you display additional text.  Note, however, that the links don’t work.
+
+In the JavaScript file, add an event handler for the ready event method.
+
+Within the function for the ready event handler, code an event handler for the toggle event method of the <a> elements.
+
+The first time one of those elements is clicked, the event handler should display the text in the division above that element, and it should change the text for the link from “Show more” to “Show less”. The second time an <a> element is clicked, the event handler should hide the text in the division above that element, and it should change the text for the link from “Show less” to “Show more”. */
