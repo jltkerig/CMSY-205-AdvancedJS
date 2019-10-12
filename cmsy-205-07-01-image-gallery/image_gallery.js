0da1b027dc798fec6dbaf61fc1036e575975a408
Jamie <jltsantara@gmail.com>
2019-10-12 12:59:05 -0400

    homework update
    finished image gallery

--- cmsy-205-07-01-image-gallery/image_gallery.js
+++ cmsy-205-07-01-image-gallery/image_gallery.js
@@ -1,20 +1,41 @@
-/* 
-All necessary HTML, CSS, and image files for this application are provided in a zip format. You’ll also find an empty JavaScript file named image_gallery.js.  You can add your code to this file. 
+ /* 
+1. Add an event handler for the ready event method.   $(document).ready(function()  ?
 
-In the JavaScript file, add an event handler for the ready event method.
+2. Use the each method to run a function for each <a> element in the unordered list of items. 
 
-Use the each method to run a function for each <a> element in the unordered list of items. Then, add jQuery code that gets the URL and caption for each image and preloads the image. You can get the URL from the href attribute of the <a> element, and you can get the caption from the title attribute.
+3. Then, add jQuery code that gets the URL and caption for each image and preloads the image. 
+You can get the URL from the href attribute of the <a> element, and you can get the caption from the title attribute.
 
-Add an event handler for the click event of each link. The function for this event handler should accept a parameter named evt. The jQuery code for this event handler should display the image and caption for the link that was clicked. In addition, it should use the evt parameter to cancel the default action of the link.
+4. Add an event handler for the click event of each link. The function for this event handler should accept a parameter named evt. The jQuery code for this event handler should display the image and caption for the link that was clicked. In addition, it should use the evt parameter to cancel the default action of the link.
 
-Add a jQuery statement that moves the focus to the first link on the page when the page is loaded.
+5. Add a jQuery statement that moves the focus to the first link on the page when the page is loaded.
+
+https://dzone.com/articles/using-the-jquery-each-function
 */
 
-"use strict";
-var $ = function(id) {return document.getElementById(id); };
-window.onload = function() {
+ 
+$(document).ready(function() {  
+ 
+ 
+    $( "#image_list a" ).each(function() { 
+        var preload1 = new Image();                 // make var preload a new image
+        preload1.src = $(this).attr("href");       //href desistation is set to var 
+    });
     
     
-    
-    
-};
+    $("#image_list a").click(function(evt){ //on click, make function event apply to id image list 'a'
+        evt.preventDefault();
+        
+        var newHREF = $(this).attr("href");                //set oldURL to current href
+        $("#image").attr("src", newHREF);       // had to switch "href" to "src" for it to work
+        
+        var caption = $(this).attr("title");
+        $("#caption").text(caption);
+        
+        
+        
+        }); //end click function
+ //focus
+  $("li:first-child a").focus();
+ 
+});//end document ready
