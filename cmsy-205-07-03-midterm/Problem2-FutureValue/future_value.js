f6a6b793b2b99a1d87eeab41cb9393faa688c2bc
Jamie <jltsantara@gmail.com>
2019-10-13 00:27:35 -0400

    problem 2 update
    1/2 done

--- cmsy-205-07-03-midterm/Problem2-FutureValue/future_value.js
+++ cmsy-205-07-03-midterm/Problem2-FutureValue/future_value.js
@@ -1,19 +1,26 @@
 var $ = function (id) {
     return document.getElementById(id);
+} 
+
+function clearRate() {
+    $('rate').value = ''
 }
+
 var calculateClick = function () {
     var investment = parseFloat( $("investment").value );
     var annualRate = parseFloat( $("rate").value );
     var years = parseInt( $("years").value );
-	
-	if (isNaN(investment) || investment < 100 || investment > 100000) {
-		alert("Investment must be an integer from 100 - 100,000.");
+if (isNaN(investment) || investment < 100 || investment > 100000) {
+		document.getElementById("investError").innerHTML = "Must be an integer from 100 - 100,000.";
 	} 
 	else if(isNaN(annualRate) || annualRate < .1 || annualRate > 12) {
-		alert("Annual rate must be a value from .1 - 12.");
+		document.getElementById("rateError").innerHTML = "Must be a value from .1 - 12.";
+        document.getElementById("investError").innerHTML = "";
+ 
 	}
 	else if(isNaN(years) || years < 1 || years > 50) {
-		alert("Years must be an integer from 1 - 50.");
+		document.getElementById("yearsError").innerHTML = "Must be an integer from 1 - 50.";
+        document.getElementById("rateError").innerHTML = "";
 	}
 	// if all entries are valid, calulate future value
 	else {
@@ -22,9 +29,11 @@ var calculateClick = function () {
 			futureValue += futureValue * annualRate / 100;
 		}
 		$("future_value").value = futureValue.toFixed();
+        document.getElementById("yearsError").innerHTML = "";
 	} 
 }
 window.onload = function () {
     $("calculate").onclick = calculateClick;
     $("investment").focus();
+    $("rate").ondblclick = clearRate();
 }
